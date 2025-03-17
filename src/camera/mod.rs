6df0a222654eb648e7d299ebbe9d7e5e7027815d
Pybounce <pybounce@protonmail.com>
2025-03-17 05:13:43 +0000

    camera follows and rotates around player

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,35 +1,57 @@
 
-use bevy::prelude::*;
+use bevy::{input::mouse::MouseMotion, prelude::*};
 
 use crate::player::components::Player;
+
+#[derive(Component)]
+pub struct CameraController {
+    pub min_pitch: f32,
+    pub max_pitch: f32,
+    pub x_speed: f32,
+    pub y_speed: f32,
+    pub distance: f32
+}
+
+impl Default for CameraController {
+    fn default() -> Self {
+        Self { 
+            min_pitch: -1.0, 
+            max_pitch: 0.0, 
+            x_speed: 0.01, 
+            y_speed: 0.01, 
+            distance: 20.0
+        }
+    }
+}
 
 pub fn spawn_camera(
     mut commands: Commands
 ) {
     commands.spawn((
         Camera3d::default(),
-        Transform::from_translation(Vec3::new(0.0, 10.0, 10.0))
+        Transform::from_translation(Vec3::new(0.0, 10.0, 10.0)),
+        CameraController::default()
     ));
 }
 
 pub fn move_camera(
-    mut camera_query: Query<&mut Transform, (With<Camera3d>, Without<Player>)>,
+    mut camera_query: Query<(&mut Transform, &CameraController), (With<Camera3d>, Without<Player>)>,
     player_query: Query<&Transform, With<Player>>,
-    time: Res<Time>
+    mut mouse_motion_events: EventReader<MouseMotion>
 ) {
-    let mut ct = camera_query.single_mut();
+    let (mut ct, cam_con) = camera_query.single_mut();
     let pt = player_query.get_single();
     match pt {
         Ok(pt) => {
-            let range = 5.0;
-            let distance = (ct.translation.distance(pt.translation) - range).max(0.0);
-
-            let speed = distance * 2.5;
-            let dir = (pt.translation - ct.translation).normalize_or_zero();
-
-            let delta = time.delta_secs() * speed * dir;
-            //ct.translation += delta;
-            ct.look_at(pt.translation, Vec3::Y);
+            for event in mouse_motion_events.read() {
+                let (yaw, pitch, roll) = ct.rotation.to_euler(EulerRot::YXZ);
+                ct.rotation = Quat::from_euler(EulerRot::YXZ, 
+                    yaw - event.delta.x * cam_con.x_speed, 
+                    (pitch - (event.delta.y * cam_con.y_speed)).clamp(cam_con.min_pitch, cam_con.max_pitch), 
+                    roll
+                );
+            }
+            ct.translation = pt.translation - ct.forward() * cam_con.distance;
         }
         Err(_) => (),
     }
