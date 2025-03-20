ae8ad139be4d3685d281fbc9c677a35d7b4f7fa2
Pybounce <pybounce@protonmail.com>
2025-03-20 02:29:02 +0000

    temp floor

--- src/stage/systems.rs
+++ src/stage/systems.rs
@@ -17,7 +17,8 @@ pub fn spawn_temp_stage(
 ) {
 
     build_bounce_pad(&mut commands, &server, &shared_assets, Vec3::new(0.0, 1.0, 0.0));
-    let a = build_floor_large(&mut commands, &server, &shared_assets, Vec3::ZERO);
+    build_floor_large(&mut commands, &server, &shared_assets, Vec3::ZERO);
+    build_floor_large(&mut commands, &server, &shared_assets, Vec3::new(30.0, -10.0, 0.0));
     build_pillar_m(&mut commands, &server, &shared_assets, Vec3::new(30.0, 0.0, 0.0));
     build_pillar_m(&mut commands, &server, &shared_assets, Vec3::new(40.0, 0.0, 10.0));
 
